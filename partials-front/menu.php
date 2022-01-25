ea3a53df38dfdbaa3e77e340b7ec6068c58f5493
arpitsingh2186 <89304921+arpitsingh2186@users.noreply.github.com>
2022-01-25 23:45:29 +0530

    Update menu.php

--- partials-front/menu.php
+++ partials-front/menu.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['lat']) && isset($_SESSION['lon'])) {
 	<title>GroFood Bae.</title>
 	<link rel="shortcut icon" type="image/x-icon" href="css/logo.jpeg"></link>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
